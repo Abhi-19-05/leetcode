c3caece46462a7a9e1431a24ef08ce3c047140f4
Abhinandan Pakhare <abhinandanpakhare@gmail.com>
2026-07-09 20:59:27 +0530

    Time: 273 ms (82.22%) | Memory: 0B (100.00%) - LeetSync

---
+++ 176-second-highest-salary/second-highest-salary.sql
@@ -0,0 +1,11 @@
+# Write your MySQL query statement below
+select  
+case when max(salary) =null then null
+     else max(salary)
+  end as SecondHighestSalary 
+from Employee
+where salary not in (
+    select max(salary) 
+    from Employee
+)
+order by salary 
