d14e48c530d0a6daa90782ec825c2e5863dc71d4
Abhinandan Pakhare <abhinandanpakhare@gmail.com>
2026-07-31 12:00:15 +0530

    Time: 1408 ms (18.12%) | Memory: 0B (100.00%) - LeetSync

--- 1174-sales-analysis-iii/sales-analysis-iii.sql
+++ 1174-sales-analysis-iii/sales-analysis-iii.sql
@@ -4,3 +4,12 @@ from Product as P
 join Sales as S on S.product_id=P.product_id
 group by P.product_id
 having min(sale_date) >='2019-01-01' and max(sale_date)<='2019-03-31' 
+
+-- SELECT p.product_id, p.product_name
+-- FROM Product p
+-- WHERE p.product_id NOT IN (
+--     SELECT product_id
+--     FROM Sales
+--     WHERE sale_date < '2019-01-01'
+--        OR sale_date > '2019-03-31'
+-- )
