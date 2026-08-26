fb0c9ec26424b636481e05ce00fe1a309a1d682d
Abhinandan Pakhare <abhinandanpakhare@gmail.com>
2026-08-26 15:45:44 +0530

    Time: 336 ms (37.11%) | Memory: 0B (100.00%) - LeetSync

--- 4195-find-users-with-high-token-usage/find-users-with-high-token-usage.sql
+++ 4195-find-users-with-high-token-usage/find-users-with-high-token-usage.sql
@@ -1,14 +1,26 @@
 # Write your MySQL query statement below
-select user_id, 
-       count(prompt) as prompt_count,
-       round(sum(tokens) / count(tokens), 2) as avg_tokens 
+
+-- select user_id, 
+--        count(prompt) as prompt_count,
+--        round(sum(tokens) / count(tokens), 2) as avg_tokens 
+-- from prompts
+-- where user_id in (
+--     select user_id
+--     from prompts 
+--     group by user_id
+--     having count(*) >= 3
+-- )
+-- group by user_id
+-- having max(tokens) > sum(tokens) / count(tokens)
+-- order by avg_tokens desc, user_id asc;
+
+select
+    user_id,
+    count(*) as prompt_count,
+    round(avg(tokens), 2) as avg_tokens
 from prompts
-where user_id in (
-    select user_id
-    from prompts 
-    group by user_id
-    having count(*) >= 3
-)
 group by user_id
-having max(tokens) > sum(tokens) / count(tokens)
+having count(*) >= 3
+   and max(tokens) > avg(tokens)
 order by avg_tokens desc, user_id asc;
+
