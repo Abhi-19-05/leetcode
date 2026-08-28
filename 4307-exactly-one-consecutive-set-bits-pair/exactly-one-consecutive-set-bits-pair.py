755969e9a2cc6ccd5e501f2ad70c4725ae204105
Abhinandan Pakhare <abhinandanpakhare@gmail.com>
2026-08-28 14:27:08 +0530

    Time: 3 ms (20.95%) | Memory: 19.4 MB (11.94%) - LeetSync

---
+++ 4307-exactly-one-consecutive-set-bits-pair/exactly-one-consecutive-set-bits-pair.py
@@ -0,0 +1,10 @@
+class Solution:
+    def consecutiveSetBits(self, n: int) -> bool:
+        s = bin(n)[2:]
+        
+        count = 0
+        for i in range(len(s) - 1):
+            if s[i] == '1' and s[i + 1] == '1':
+                count += 1
+        
+        return count == 1
